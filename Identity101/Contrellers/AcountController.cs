ad11117d19715beaaccd2d042af11a5c133c7ea4
Murat AKTAŞ <Murat.Aktaas@hotmail.com>
2022-04-13 13:24:01 +0300

    ConfirmResetPassword

--- Identity101/Contrellers/AcountController.cs
+++ Identity101/Contrellers/AcountController.cs
@@ -8,11 +8,11 @@ using Identity101.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.WebUtilities;
 
 namespace Identity101.Controllers;
 
+[AllowAnonymous]
 public class AccountController : Controller
 {
     private readonly UserManager<ApplicationUser> _userManager;
@@ -161,6 +161,7 @@ public class AccountController : Controller
         {
             //TODO: 2fa yönlendirmesi yapılacak
         }
+
         ModelState.AddModelError(string.Empty, "Kullanıcı adı veya şifre hatalı");
         return View(model);
     }
@@ -170,12 +171,92 @@ public class AccountController : Controller
     {
         await _signInManager.SignOutAsync();
         return RedirectToAction("Index", "Home");
-
     }
 
     public IActionResult AccessDenied()
     {
         return View();
+    }
 
+    [HttpGet]
+    public IActionResult ResetPassword()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> ResetPassword(string email)
+    {
+        var user = await _userManager.FindByEmailAsync(email);
+
+        if (user == null)
+        {
+            ViewBag.Message = "Mailinize Şifre güncelleme yönergemiz gönderilmiştir";
+        }
+        else
+        {
+            var code = await _userManager.GeneratePasswordResetTokenAsync(user);
+            code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+            var callbackUrl = Url.Action("ConfirmResetPassword", "Account", new { userId = user.Id, code = code },
+                protocol: Request.Scheme);
+
+            var emailMessage = new MailModel()
+            {
+                To = new List<EmailModel>
+                {
+                    new EmailModel()
+                        { Adress = user.Email, Name = user.UserName }
+                },
+                Body =
+                    $"Please reset your password by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.",
+                Subject = "Reset Password"
+            };
+
+            await _emailService.SendMailAsync(emailMessage);
+
+            ViewBag.Message = "Mailinize Şifre güncelleme yönergemiz gönderilmiştir";
+        }
+
+        return View();
+    }
+    public IActionResult ConfirmResetPassword(string userId, string code)
+    {
+        if (string.IsNullOrEmpty(userId) || string.IsNullOrEmpty(code))
+        {
+            return BadRequest("Hatalı istek");
+        }
+
+        ViewBag.Code = code;
+        ViewBag.UserId = userId;
+        return View();
+    }
+    [HttpPost]
+    public async Task<IActionResult> ConfirmResetPassword(ResetPasswordViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(model);
+        }
+
+        var user = await _userManager.FindByIdAsync(model.UserId);
+
+        if (user == null)
+        {
+            ModelState.AddModelError(string.Empty, "Kullanıcı bulunamadı");
+            return View();
+        }
+        var code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(model.Code));
+        var result = await _userManager.ResetPasswordAsync(user, code, model.NewPassword);
+
+        if (result.Succeeded)
+        {
+            //email gönder
+            TempData["Message"] = "Şifre değişikliğiniz gerçekleştirilmiştir";
+            return View();
+        }
+
+        var message = string.Join("<br>", result.Errors.Select(x => x.Description));
+        TempData["Message"] = message;
+        return View();
     }
 }
---
+++ Identity101/ViewModels/ResetPasswordViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace Identity101.ViewModels
+{
+    public class ResetPasswordViewModel
+    {
+
+        [Required(ErrorMessage = "Şifre alanı gerekiyor")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Şifreniz minimum 6 karakterli olmalıdır!")]
+        [Display(Name = "Yeni Şifre")]
+        [DataType(DataType.Password)]
+
+        public string NewPassword { get; set; }
+        [Required(ErrorMessage = "Şifre alanı gerekiyor")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Yeni Şifre Tekrar")]
+        [Compare(nameof(NewPassword),ErrorMessage = "Şifreler uyuşmuyor")]
+        public string ConfirmNewPassword { get; set; }
+        public string Code { get; set; }
+        public string UserId { get; set; }
+    }
+
+}
