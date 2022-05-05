d6b452a20c67741bb64cb4128b854020e043b8e6
Murat AKTAŞ <Murat.Aktaas@hotmail.com>
2022-05-05 15:10:14 +0300

    jquery .

--- AdminTemplate/FrontEnd/JavaScript/33.js
+++ AdminTemplate/FrontEnd/JavaScript/33.js
@@ -1,21 +1,21 @@
-function RedValue(){
-    var red = document.getElementById("range-red").value;
-    console.log("Red: "+red)
-}
+// function RedValue(){
+//     var red = document.getElementById("range-red").value;
+//     console.log("Red: "+red)
+// }
 
-function GreenValue(){
-    var green = document.getElementById("range-green").value;
-    console.log("Green: "+green)
-}
+// function GreenValue(){
+//     var green = document.getElementById("range-green").value;
+//     console.log("Green: "+green)
+// }
 
-function BlueValue(){
-    var blue = document.getElementById("range-blue").value;
-    console.log("Blue: "+blue)
-}
-
-
-src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCnBDK9iDufYofxhksklBgDWOcyTsCKEMs&callback=initMap&libraries=places"
+// function BlueValue(){
+//     var blue = document.getElementById("range-blue").value;
+//     console.log("Blue: "+blue)
+// }
 
 
-AIzaSyD8r-bTE8Ou4bH2nU000o-MpwsEQpZNikQ&callback=initMap
-//AIzaSyCnBDK9iDufYofxhksklBgDWOcyTsCKEMs & callback=initMap & libraries
+// src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCnBDK9iDufYofxhksklBgDWOcyTsCKEMs&callback=initMap&libraries=places"
+
+
+// // AIzaSyD8r-bTE8Ou4bH2nU000o-MpwsEQpZNikQ&callback=initMap
+// //AIzaSyCnBDK9iDufYofxhksklBgDWOcyTsCKEMs & callback=initMap & libraries
