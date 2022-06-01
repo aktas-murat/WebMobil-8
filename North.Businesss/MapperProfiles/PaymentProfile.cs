72fc46f489c93fe734a60a2aa13e3d9d080a8f29
Murat AKTAŞ <Murat.Aktaas@hotmail.com>
2022-06-01 16:40:05 +0300

    Payment Service

--- North.Businesss/MapperProfiles/PaymentProfile.cs
+++ North.Businesss/MapperProfiles/PaymentProfile.cs
@@ -1,18 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using Iyzipay.Model;
 using North.Core.Payments;
 
 namespace North.Businesss.MapperProfiles
 {
-    internal class PaymentProfile 
+    public class PaymentProfile : Profile
     {
         public PaymentProfile()
         {
-         
+            CreateMap<CardModel, PaymentCard>().ReverseMap();
+            CreateMap<BasketModel, BasketItem>().ReverseMap();
+            CreateMap<AddressModel, Address>().ReverseMap();
+            CreateMap<CustomerModel, Buyer>().ReverseMap();
+            CreateMap<InstallmentPriceModel, InstallmentPrice>().ReverseMap();
+            CreateMap<InstallmentModel, InstallmentDetail>().ReverseMap();
+            CreateMap<PaymentResponseModel, Payment>().ReverseMap();
         }
     }
-
 }
--- North.Businesss/Services/Payment/IPaymentService.cs
+++ North.Businesss/Services/Payment/IPaymentService.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using North.Core.Payments;
+﻿using North.Core.Payments;
 
 namespace North.Businesss.Services.Payment
 {
-   public interface IPaymentService
+    public interface IPaymentService
     {
         InstallmentModel CheckInstallments(string binNumber, decimal price);
-        
+        PaymentResponseModel Pay(PaymentModel model);
     }
 }
