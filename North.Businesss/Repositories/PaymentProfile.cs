42992fb0a3dd406b9bf91d51dbe2bee8899da30f
Murat AKTAŞ <Murat.Aktaas@hotmail.com>
2022-06-01 16:12:06 +0300

    codefirst/payment models

---
+++ North.Businesss/Repositories/PaymentProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using North.Core.Payments;
+
+namespace North.Businesss.MapperProfiles
+{
+    internal class PaymentProfile 
+    {
+        public PaymentProfile()
+        {
+         
+        }
+    }
+
+}
---
+++ North.Businesss/Services/Payment/IPaymentService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using North.Core.Payments;
+
+namespace North.Businesss.Services.Payment
+{
+   public interface IPaymentService
+    {
+        InstallmentModel CheckInstallments(string binNumber, decimal price);
+        
+    }
+}
